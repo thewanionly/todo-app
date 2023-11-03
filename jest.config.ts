8b3b505d3025d2c3f25bfdc42e70974dc2eb154a
thewanionly <pelloani@gmail.com>
2023-11-04 04:07:14 +0800

    chore: setup testing
    use jest, react testing library

---
+++ jest.config.ts
@@ -0,0 +1,31 @@
+import nextJest from 'next/jest.js';
+
+import type { Config } from '@jest/types';
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+});
+
+// Add any custom config to be passed to Jest
+const config: Config.InitialOptions = {
+  // Add more setup options before each test is run
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jest-environment-jsdom',
+};
+
+const jestConfig = async () => {
+  const nextJestConfig = await createJestConfig(config)();
+
+  return {
+    ...nextJestConfig,
+    // TODO: Add when svgr is needed
+    // // Set up this way to fix svgr issue in Jest. Source: https://github.com/vercel/next.js/discussions/42535#discussion-4542627
+    // moduleNameMapper: {
+    //   ...nextJestConfig.moduleNameMapper,
+    //   '\\.svg$': '<rootDir>/src/tests/__mocks__/svg.ts',
+    // },
+  };
+};
+
+export default jestConfig;
