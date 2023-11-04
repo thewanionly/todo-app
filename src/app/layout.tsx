72028260c60fcca3161dd977a46fdebc9e68660e
thewanionly <pelloani@gmail.com>
2023-11-05 02:57:42 +0800

    feat: add Josefin Sans font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Josefin_Sans } from 'next/font/google';
 
 import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+const josefinSans = Josefin_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 export const metadata: Metadata = {
   title: 'Todo App',
@@ -13,7 +16,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={josefinSans.className}>{children}</body>
     </html>
   );
 }
