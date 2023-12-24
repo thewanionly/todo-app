400dad360f483ccab8ca567091e1e36a782083fa
thewanionly <pelloani@gmail.com>
2023-12-24 15:48:36 +0800

    test: add missing Button component test cases

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,13 +1,26 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import { Button } from '.';
 
 describe('Button', () => {
-  it('displays button', () => {
-    const child = 'hello';
-    render(<Button> {child}</Button>);
+  it('displays button with the passed children', () => {
+    const buttonChild = 'hello';
+    render(<Button>{buttonChild}</Button>);
 
     const button = screen.getByRole('button');
     expect(button).toBeInTheDocument();
+    expect(button).toHaveAccessibleName(buttonChild);
+  });
+
+  it('calls the function passed in the `onClick` prop', async () => {
+    const buttonChild = 'Click me';
+    const onClickHandler = jest.fn();
+    render(<Button onClick={onClickHandler}>{buttonChild}</Button>);
+
+    const button = screen.getByRole('button', { name: buttonChild });
+    userEvent.click(button);
+
+    await waitFor(() => expect(onClickHandler).toHaveBeenCalled());
   });
 });
