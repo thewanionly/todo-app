50fa1a4b404331cac788dbcca699ca2f73b761af
thewanionly <pelloani@gmail.com>
2024-01-03 03:11:40 +0800

    feat: update useDarkMode logic to include browser preference and local storage support

--- jest.setup.ts
+++ jest.setup.ts
@@ -5,3 +5,17 @@ Object.defineProperty(globalThis, 'crypto', {
     randomUUID: () => Math.random(),
   },
 });
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // Deprecated
+    removeListener: jest.fn(), // Deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
--- src/components/DarkModeToggle/DarkModeToggle.hooks.ts
+++ src/components/DarkModeToggle/DarkModeToggle.hooks.ts
@@ -1,10 +1,44 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
-export const useDarkMode = (initialValue = false) => {
-  const [isDarkMode, setIsDarkMode] = useState(initialValue);
+const DARK_MODE_LOCAL_STORAGE_KEY = 'isDarkMode';
+
+export const useDarkMode = (defaultValue?: boolean) => {
+  // Get user's browser theme preference
+  const isDarkModeInColorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+  // Dark mode's initial value can be retrieved from various sources.
+  // Here's the priority:
+  //  [1] app's dark mode toggle set in local storage
+  //  [2] app's dark mode toggle default value
+  //  [3] user's browser theme preference
+  const initializeDarkModeState = useCallback((): boolean => {
+    const initialValue = defaultValue ?? isDarkModeInColorScheme;
+
+    if (typeof window === 'undefined') return initialValue;
+
+    try {
+      // Get dark mode value from local storage
+      const isDarkModeInLocalStorage = window.localStorage.getItem(DARK_MODE_LOCAL_STORAGE_KEY);
+
+      return JSON.parse(isDarkModeInLocalStorage ?? 'null') ?? initialValue;
+    } catch (error) {
+      console.warn(`Error reading localStorage key “${DARK_MODE_LOCAL_STORAGE_KEY}”:`, error);
+      return initialValue;
+    }
+  }, [defaultValue, isDarkModeInColorScheme]);
+
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(initializeDarkModeState);
 
   const toggleDarkMode = () => {
-    setIsDarkMode((prevValue) => !prevValue);
+    const newValue = !isDarkMode;
+
+    // Set new isDarkMode value to state
+    setIsDarkMode(newValue);
+
+    // Store new isDarkMode value to local storage
+    if (typeof window === 'undefined') return;
+
+    window.localStorage.setItem(DARK_MODE_LOCAL_STORAGE_KEY, JSON.stringify(newValue));
   };
 
   return {
