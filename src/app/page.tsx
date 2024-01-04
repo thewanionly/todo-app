3527d7ffca62fa9e94944bd41680d0f1edf3c7b8
thewanionly <pelloani@gmail.com>
2024-01-05 04:09:39 +0800

    feat: update width styles of Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,9 @@ export default function Home() {
   return (
     <>
       <Header
-        className={twMerge(`mx-auto mt-12 min-w-[300px] max-w-xl px-6 md:mt-14 lg:mt-16 xl:mt-20`)}
+        className={twMerge(
+          `mx-auto mt-12 w-[87.2%] min-w-[300px] max-w-screen-xl px-6 md:mt-14 lg:mt-16 xl:mt-20`
+        )}
       />
     </>
   );
