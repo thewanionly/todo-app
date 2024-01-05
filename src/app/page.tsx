0a79256e70dbeb9c52091fc934d9ac0073c6d38c
thewanionly <pelloani@gmail.com>
2024-01-06 03:14:14 +0800

    feat: show TodoList in the application

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,36 @@
 import { twMerge } from 'tailwind-merge';
 
 import { Header } from '@/components/Header';
+import { TodoList } from '@/components/TodoList';
+import { useTodoList } from '@/components/TodoList/TodoList.hooks';
 
 export default function Home() {
+  const {
+    todoListRef,
+    items,
+    onAddItem,
+    onItemValueChange,
+    onItemCompletedChange,
+    onDeleteItem,
+    onDeleteCompletedItems,
+  } = useTodoList([]); // TODO: get initial from local storage
+
   return (
     <>
       <Header
         className={twMerge(
-          `mx-auto mt-12 w-[87.2%] min-w-[300px] max-w-screen-xl px-6 md:mt-14 lg:mt-16 xl:mt-20`
+          `mx-auto mt-12 w-[87.2%] min-w-[300px] max-w-xl md:mt-14 lg:mt-16 xl:mt-20`
         )}
+      />
+      <TodoList
+        ref={todoListRef}
+        className="relative z-10 mx-auto mt-10 w-[87.2%] min-w-[300px] max-w-xl md:mt-12"
+        items={items}
+        onAddItem={onAddItem}
+        onItemValueChange={onItemValueChange}
+        onItemCompletedChange={onItemCompletedChange}
+        onDeleteItem={onDeleteItem}
+        onDeleteCompletedItems={onDeleteCompletedItems}
       />
     </>
   );
