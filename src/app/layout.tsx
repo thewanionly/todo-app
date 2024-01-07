167af760f0e1a74dafeac126a54906037a529f9d
thewanionly <pelloani@gmail.com>
2024-01-07 22:06:02 +0800

    feat: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,26 @@ import './globals.css';
 export const metadata: Metadata = {
   title: 'Todo App',
   description: 'A "To do" list application',
+  icons: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      url: '/favicon/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      url: '/favicon/favicon-16x16.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      url: '/favicon/apple-touch-icon.png',
+    },
+  ],
+  manifest: '/favicon/site.webmanifest',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
