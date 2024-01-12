95a2464b48d72fc542d29dcb2323b7bba1f070d3
thewanionly <pelloani@gmail.com>
2024-01-13 03:59:05 +0800

    refactor: create Home component which holds client components

---
+++ src/app/Home/Home.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import { twMerge } from 'tailwind-merge';
+
+import { Header } from '@/components/Header';
+import { TodoList } from '@/components/TodoList';
+import { useTodoList } from '@/components/TodoList/TodoList.hooks';
+
+export const Home = () => {
+  const {
+    todoListRef,
+    items,
+    onAddItem,
+    onItemValueChange,
+    onItemCompletedChange,
+    onDeleteItem,
+    onDeleteCompletedItems,
+  } = useTodoList([]);
+
+  return (
+    <>
+      <Header
+        className={twMerge(
+          `mx-auto mt-12 w-[87.2%] min-w-[300px] max-w-xl md:mt-14 lg:mt-16 xl:mt-20`
+        )}
+      />
+      <TodoList
+        ref={todoListRef}
+        className="relative z-10 mx-auto mt-10 w-[87.2%] min-w-[300px] max-w-xl md:mt-12"
+        items={items}
+        onAddItem={onAddItem}
+        onItemValueChange={onItemValueChange}
+        onItemCompletedChange={onItemCompletedChange}
+        onDeleteItem={onDeleteItem}
+        onDeleteCompletedItems={onDeleteCompletedItems}
+      />
+    </>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,5 @@
-'use client';
+import { Home } from './Home';
 
-import { twMerge } from 'tailwind-merge';
-
-import { Header } from '@/components/Header';
-import { TodoList } from '@/components/TodoList';
-import { useTodoList } from '@/components/TodoList/TodoList.hooks';
-
-export default function Home() {
-  const {
-    todoListRef,
-    items,
-    onAddItem,
-    onItemValueChange,
-    onItemCompletedChange,
-    onDeleteItem,
-    onDeleteCompletedItems,
-  } = useTodoList([]);
-
-  return (
-    <>
-      <Header
-        className={twMerge(
-          `mx-auto mt-12 w-[87.2%] min-w-[300px] max-w-xl md:mt-14 lg:mt-16 xl:mt-20`
-        )}
-      />
-      <TodoList
-        ref={todoListRef}
-        className="relative z-10 mx-auto mt-10 w-[87.2%] min-w-[300px] max-w-xl md:mt-12"
-        items={items}
-        onAddItem={onAddItem}
-        onItemValueChange={onItemValueChange}
-        onItemCompletedChange={onItemCompletedChange}
-        onDeleteItem={onDeleteItem}
-        onDeleteCompletedItems={onDeleteCompletedItems}
-      />
-    </>
-  );
+export default function HomPage() {
+  return <Home />;
 }
