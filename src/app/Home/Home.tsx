cec85b194e3da3066d1ad5364945ad255d4a9137
thewanionly <pelloani@gmail.com>
2024-03-04 04:17:15 +0800

    refactor: place Header in layout

--- src/app/Home/Home.tsx
+++ src/app/Home/Home.tsx
@@ -1,8 +1,5 @@
 'use client';
 
-import { twMerge } from 'tailwind-merge';
-
-import { Header } from '@/components/Header';
 import { TodoList } from '@/components/TodoList';
 import { useTodoList } from '@/components/TodoList/TodoList.hooks';
 
@@ -18,24 +15,17 @@ export const Home = () => {
   } = useTodoList([]);
 
   return (
-    <>
-      <Header
-        className={twMerge(
-          `mx-auto mt-12 w-[87.2%] min-w-[300px] max-w-xl md:mt-14 lg:mt-16 xl:mt-20`
-        )}
+    <main>
+      <TodoList
+        ref={todoListRef}
+        className="relative z-10 mx-auto mt-10 w-[87.2%] min-w-[300px] max-w-xl md:mt-12"
+        items={items}
+        onAddItem={onAddItem}
+        onItemValueChange={onItemValueChange}
+        onItemCompletedChange={onItemCompletedChange}
+        onDeleteItem={onDeleteItem}
+        onDeleteCompletedItems={onDeleteCompletedItems}
       />
-      <main>
-        <TodoList
-          ref={todoListRef}
-          className="relative z-10 mx-auto mt-10 w-[87.2%] min-w-[300px] max-w-xl md:mt-12"
-          items={items}
-          onAddItem={onAddItem}
-          onItemValueChange={onItemValueChange}
-          onItemCompletedChange={onItemCompletedChange}
-          onDeleteItem={onDeleteItem}
-          onDeleteCompletedItems={onDeleteCompletedItems}
-        />
-      </main>
-    </>
+    </main>
   );
 };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next';
 
+import { twMerge } from 'tailwind-merge';
+
 import { DarkModeProvider } from '@/components/DarkModeToggle/DarkModeProvider';
+import { Header } from '@/components/Header';
 import { josefinSans } from '@/lib/fonts';
 
 import './globals.css';
@@ -34,7 +37,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className={`${josefinSans.variable} bg-body-bg`} suppressHydrationWarning>
       <body>
-        <DarkModeProvider>{children}</DarkModeProvider>
+        <DarkModeProvider>
+          <Header
+            className={twMerge(
+              `mx-auto mt-12 w-[87.2%] min-w-[300px] max-w-xl md:mt-14 lg:mt-16 xl:mt-20`
+            )}
+          />
+          {children}
+        </DarkModeProvider>
       </body>
     </html>
   );
